7f28d33006c421b206f95e1c043fea1496a93a75
Abd. Rauf <bdsurel19@gmail.com>
2023-05-28 20:15:36 +0700

    Add TC-0004

---
+++ Jubelio-Login-TestAutomation/TC-0004.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time
+
+options = Options()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                          options=options)
+
+driver.maximize_window()
+driver.get("https://app2.jubelio.com/login")
+time.sleep(3)
+
+driver.find_element(By.CLASS_NAME, "ladda-label").click()
