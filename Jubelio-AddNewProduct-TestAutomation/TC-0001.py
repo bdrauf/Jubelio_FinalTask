f34653277f5456dbd664231b44c2b6cd87754ee1
Abd. Rauf <bdsurel19@gmail.com>
2023-06-02 03:15:19 +0700

    TC-0001 Edit

--- Jubelio-AddNewProduct-TestAutomation/TC-0001.py
+++ Jubelio-AddNewProduct-TestAutomation/TC-0001.py
@@ -10,8 +10,9 @@ import time
 options = Options()
 options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                          options=options)
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()), options=options
+)
 
 driver.maximize_window()
 driver.get("https://app2.jubelio.com/login")
@@ -25,13 +26,31 @@ driver.find_element(By.XPATH, "//span[text()='Barang']").click()
 driver.find_element(By.XPATH, "//span[text()='Katalog']").click()
 driver.find_element(By.XPATH, "//span[text()='Master']").click()
 time.sleep(3)
-driver.find_element(By.XPATH, "//button[@class='ladda-button btn btn-primary m-l-xs']").click()
+driver.find_element(
+    By.XPATH, "//button[@class='ladda-button btn btn-primary m-l-xs']"
+).click()
 driver.find_element(By.NAME, "item_group_name").send_keys("Milo")
-#time.sleep(3)
+# time.sleep(3)
 driver.find_element(By.CLASS_NAME, "selectivity-input").click()
-#time.sleep(3)
+# time.sleep(3)
 
-sel1=driver.find_element(By.CLASS_NAME, "selectivity-search-input")
-sel1.send_keys("Makan"+Keys.RETURN)
+sel1 = driver.find_element(By.CLASS_NAME, "selectivity-search-input")
+sel1.send_keys("Makan" + Keys.RETURN)
 time.sleep(3)
 sel1.send_keys(Keys.RETURN)
+
+driver.find_element(By.NAME, "brand_name").send_keys("Milo")
+
+driver.find_element(By.NAME, "item_code").send_keys("P0003")
+
+driver.find_element(By.NAME, "barcode").send_keys("33234567890")
+
+driver.find_element(
+    By.CSS_SELECTOR,
+    "div.RichTextEditor__block___2Vs_D.RichTextEditor__paragraph___3NTf9",
+).send_keys(
+    "lorem ipusm sit amet kemana ada memasuk apapun bentuknya laser kebudayan bende alah"
+)
+driver.find_element(By.CLASS_NAME, "gallery-content").send_keys(
+    "/Users/abd.rauf/Downloads/6.png"
+)
